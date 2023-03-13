d979c7af71398191711d23290551d11cf8dc2222
Nghia <nghiacao1997@gmail.com>
2023-03-13 21:43:31 +0700

    fix cors

--- index.js
+++ index.js
@@ -9,17 +9,18 @@ const port = process.env.PORT;
 
 app.use(express.json());
 
-const whitelist = ["http://localhost:3000", "http://localhost:5001"];
-const corsOptions = {
-    origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-        callback(null, true)
-      } else {
-        callback(new Error('Not allowed by CORS'))
-      }
-    }
-}
-app.use(cors(corsOptions));
+// const whitelist = ["http://localhost:3000", "http://localhost:5001"];
+// const corsOptions = {
+//     origin: function (origin, callback) {
+//       if (whitelist.indexOf(origin) !== -1) {
+//         callback(null, true)
+//       } else {
+//         callback(new Error('Not allowed by CORS'))
+//       }
+//     }
+// }
+// app.use(cors(corsOptions));
+app.use(cors())
 
 connectToDb();
 
